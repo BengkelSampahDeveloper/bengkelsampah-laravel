1d3b5f9a19235cf087ff8154eed9d382f05fc07e
JackMizh <47821665+JackMizh@users.noreply.github.com>
2025-07-19 07:36:04 +0700

    Fix user-guide production build and add .htaccess for proper routing

--- user-guide/docusaurus.config.js
+++ user-guide/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://bengkelsampah.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/user-guide/',
